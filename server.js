ea659811e0bc83c2dacfdfea11cc3b26c1ba635a
vxhienvn <82212518+vxhienvn@users.noreply.github.com>
2026-06-20 20:30:35 +0000

    add meta webhook

--- server.js
+++ server.js
@@ -4,18 +4,36 @@ const app = express();
 
 app.use(express.json());
 
+const VERIFY_TOKEN = "Vietnam84@";
+
 app.get('/', (req, res) => {
     res.send('Server OK');
 });
 
-app.post('/chat', (req, res) => {
+app.get('/webhook', (req, res) => {
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
 
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        console.log('Webhook verified');
+        res.status(200).send(challenge);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+app.post('/webhook', (req, res) => {
+    console.log(JSON.stringify(req.body, null, 2));
+    res.sendStatus(200);
+});
+
+app.post('/chat', (req, res) => {
     console.log(req.body);
 
     res.json({
         reply: 'Xin chào từ Render'
     });
-
 });
 
 const PORT = process.env.PORT || 10000;
