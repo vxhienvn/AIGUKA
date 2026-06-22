cdcce1a1e4ea2d251f70946b5b0f278ac063d097
vxhienvn <82212518+vxhienvn@users.noreply.github.com>
2026-06-22 15:10:28 +0000

    fix server memory code

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ function shouldSendCarousel(customerMessage) {
     "cho ảnh", "cho anh",
     "xem mẫu", "xem mau",
     "cho xem", "gửi mẫu", "gui mau",
-    "xin mẫu", "xin mau","cho mẫu", "cho mau", "xem"
+"xin mẫu", "xin mau","cho mẫu", "cho mau", "xem", "xem combo", "xem nhà tắm", ""
 ];
     return words.some(word => msg.includes(word));
 }
